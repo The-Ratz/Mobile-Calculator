3ec8132edbc8359b57be9309c8b586fe1f13da4f
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2025-02-06 17:22:09 -0300

    feat: mobile calculator

---
+++ app/src/main/java/com/hadirahimi/calculator/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.hadirahimi.calculator
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import com.hadirahimi.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var expression = StringBuilder()
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // NoLimitScreen
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        // Inicializa os botões
+        binding.apply {
+            binding.layoutMain.children.filterIsInstance<Button>().forEach { button ->
+                button.setOnClickListener {
+                    val buttonText = button.text.toString()
+                    when {
+                        buttonText.matches(Regex("[0-9+\\-*/.]")) -> {
+                            expression.append(buttonText)
+                            tvResult.text = expression.toString()
+                        }
+                        buttonText == "=" -> {
+                            val result = evaluateExpression(expression.toString())
+                            if (result != "Erro") {
+                                tvFormula.text = expression.toString()
+                                expression.clear()
+                                expression.append(result)
+                                tvResult.text = result
+                            } else {
+                                tvResult.text = "Erro"
+                            }
+                        }
+                        buttonText == "C" -> {
+                            expression.clear()
+                            tvResult.text = "0"
+                            tvFormula.text = ""
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Avaliação de Expressão usando exp4j
+    private fun evaluateExpression(expr: String): String {
+        return try {
+            val result = ExpressionBuilder(expr).build().evaluate()
+            result.toString()
+        } catch (e: Exception) {
+            "Erro"
+        }
+    }
+}
